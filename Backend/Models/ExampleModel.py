7cf632b37c60dc1bae33d2a4da22b43c735326fe
Alexandru Zvinca <alexandruzvinca@yahoo.com>
2022-02-24 19:58:27 +0200

    first commit

---
+++ Backend/Models/ExampleModel.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ExampleModel(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField(max_length=500)
+
+
+def __str__(self):
+    return self.name
