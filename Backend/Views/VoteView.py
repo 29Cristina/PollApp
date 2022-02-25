b0597605b9c921d7d2b8c390055c163b3f41de7f
Cojocaru Cristian <cojocaru.cristian20@gamil.com>
2022-02-25 20:54:19 +0200

    Changed VoteView

--- Backend/Views/VoteView.py
+++ Backend/Views/VoteView.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from Backend.models.VoteModel import VoteModel
 from Backend.Serializers.VoteSerializer import VoteSerializer
 
 
-class VoteList(generics.ListCreateAPIView):
-    queryset = VoteModel.objects.all()
-    serializer_class = VoteSerializer
+class VoteList(APIView):
+    def get(self, request, format=None):
+        votes = VoteModel.objects.all()
+        serializer = VoteSerializer(votes, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = VoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = VoteModel.objects.all()
-    serializer_class = VoteSerializer
+        queryset = VoteModel.objects.all()
+        serializer_class = VoteSerializer
