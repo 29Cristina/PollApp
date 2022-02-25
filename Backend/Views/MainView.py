33dc04ef50eb14940cfdbd974fd2a5b6c3be8fd6
Mario <mateasmario@aol.com>
2022-02-25 20:25:02 +0200

    Modified index view to rest_framework view

--- Backend/Views/MainView.py
+++ Backend/Views/MainView.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def MainView(response):
     s="/choices and choice/nr \n votes and vote/nr \n"
-    return HttpResponse(s)
+    return Response(s)
 
 
 
