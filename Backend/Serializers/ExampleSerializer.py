4810e47896a6828f3db96088dba053d7dd1a8e73
Mario <mateasmario@aol.com>
2022-03-19 14:43:53 +0200

    Removed ExampleModel from project

--- Backend/Serializers/ExampleSerializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..models.ExampleModel import ExampleModel
-
-class ExampleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ExampleModel
-        fields = "__all__"
